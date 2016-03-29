e1f563a3b08626f14bbc825e7295cce90b6be189
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-03-29 17:23:32 +0200

    [TR-003] feature for worker process

--- worker.py
+++ worker.py
@@ -10,13 +10,21 @@ Full license text is avaible at http://www.gnu.org/licenses/lgpl-3.0.html
 
 """
 
+from Thread import Thread
+
 class Worker:
 	""" Worker is the process that controls execution of tasks """
 	def __init__(self):
+		self.ttl = 5
+		self.ttl_thread = Thread()
 		pass
 
 	def setup_work(self, work_struct):
 		""" This method sets up a task for worker by reciving work structure """
+		pass
+
+	def recv_ping(self):
+		self.ttl = 5
 		pass
 
 	def do_work(self):

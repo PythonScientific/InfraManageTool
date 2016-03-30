a26dfc3c5fe336fcafc2a565aa803682cad6edd8
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-03-30 16:12:12 +0200

    [FT] msg class expanded over with netio

--- msg.py
+++ msg.py
@@ -1,8 +1,26 @@
+""" MSG class
+Basic structure for messanges between peers
+
+Author: Patryk Zabkiewicz
+Date:	2016.03.30
+
+License info:
+This software comes with NO WARRANTY. You use it at your own risk.
+Full license text is avaible at http://www.gnu.org/licenses/lgpl-3.0.html
+
+"""
 
 class MSG:
     """ Basic structure for messanges between peers """
+    id = 0                          # id of the messanges / static member
+
     def __init__(self):
-        self.id = 0
-        self.timestamp = []
-        self.data = []
-        self.sumbit = 0
+        self.timestamp = []         # timestamp
+        self.data = []              # data to be sended out
+        self.sumbit = 0             # CRC sum bit
+        self.bitout = []            # encoded bits to send out throu network
+
+    @staticmethod
+    def getID():
+        MSG.id += 1
+        return MSG.id
--- netio.py
+++ netio.py
@@ -1,7 +1,17 @@
+""" NetIO
+Reponsible for communication with other peers
+
+Author: Patryk Zabkiewicz
+Date:	2016.03.30
+
+License info:
+This software comes with NO WARRANTY. You use it at your own risk.
+Full license text is avaible at http://www.gnu.org/licenses/lgpl-3.0.html
+
+"""
 
 from queue import Queue
 from msg import MSG
-
 
 class NetIO(object):
     """ NetIO is reponsible for communication with other peers """
@@ -13,17 +23,19 @@ class NetIO(object):
 
     def send(self):
         """ single messange send to all peers """
-        pass
+        msg = self.out_buffer.get()
+        for p in self.peers: # go throu all peers
+            p.out_buffer.put(msg)
 
     def recv(self):
         """ single messange recv from peers """
-        for p in self.peers:
+        for p in self.peers: # go throu all peers
             self.in_buffer.put(p.recv())
 
     def buffer_read(self):
         """ read next messange from recive buffer """
         return self.in_buffer.get()
 
-    def buffer_send(self):
+    def buffer_send(self, msg):
         """ write next messange to send buffer """
-        pass
+        self.out_buffer.put(msg)

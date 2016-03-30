3ef4dbf5fe715e30dedffe34e304cd06c41cda8f
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-03-30 16:17:31 +0200

    [FT] peer.py

--- peer.py
+++ peer.py
@@ -1,1 +1,11 @@
 
+class Peer(object):
+    """ Implementation of the network peer """
+    def __init__(self):
+        super(Peer, self).__init__()
+        self.out_buffer = Queue()       # buffer to send out messanges
+        self.in_buffer = Queue()        # buffer to recv messanges from peer
+        pass
+
+    def send():
+        pass

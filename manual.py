ee8fe8e870b096ccd92667e956e5e70e730ca745
Patryk Zabkiewicz <pzabkiewicz@gmail.com>
2016-04-04 20:43:10 +0200

    [FT] manual entries with explanatory comment

--- manual.py
+++ manual.py
@@ -1,3 +1,15 @@
+""" Manual namespace
+All functions related to help and manual pages
+
+Author: 	Patryk Zabkiewicz
+Date:		2016.04.04
+
+License info:
+This software comes with NO WARRANTY. You use it at your own risk.
+Full license text is avaible at http://www.gnu.org/licenses/lgpl-3.0.html
+
+"""
+
 def list_help():
     print "peers\t- list all your peers"
     print
